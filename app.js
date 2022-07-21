a07694b822ee6b142435902f52f7c96ae164c75e
Milind Waghmare <milind.waghmare@vistaura.com>
2022-07-21 18:26:06 +0530

    added name in reply msg

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ const express = require('express');
 const app = express();
 const PORT =  process.env.PORT || 5000
 const {Telegraf} = require('telegraf');
-const bot = new Telegraf('5505915548:AAFICbQOcgWNTOVkZBJsGW5SJsqdjjdTY4w')
+const bot = new Telegraf(process.env.TELEGRAM_BOT_ID)
 const web_link = 'https://food-king.vercel.app/'
 app.get('/', (req, res)=>{
     res.send("welcome chatbot")
 })
 
-bot.start((ctx) => ctx.reply('Welcome to food king',{reply_markup:{keyboard:[[{text:'web app', web_app:{url: web_link}}]]}}))
+bot.start((ctx) => 
+ctx.reply(`Welcome ${ctx.message.from.first_name} to food king`,{reply_markup:{keyboard:[[{text:'web app', web_app:{url: web_link}}]]}}))
+
 
 bot.launch()
 
 app.listen(PORT, ()=>{
 
-    console.log("server is started @" + PORT)
+    console.log("server is started @ " + PORT)
 })
